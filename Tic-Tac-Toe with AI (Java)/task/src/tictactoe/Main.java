7c4134029d96129c3dca331f0b580cab1c0f5c6c
Sasa <alexandrchmelix@gmail.com>
2024-08-28 22:06:21 +0200

    gg

---
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Main.java
@@ -0,0 +1,228 @@
+package tictactoe;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.SimpleFormatter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // --- get input ----
+        Scanner scanner = new Scanner(System.in);
+        // --- set and print field ----
+        char[][] field = createField();
+        // --- starting input ----
+        String input;
+        String[] inputArr;
+        while(true){
+            System.out.println("Input command: ");
+            input = scanner.nextLine();
+            inputArr = input.split(" ");
+
+            if (inputArr[0].equals("exit")){
+                return;
+            }
+
+            if (inputArr.length == 3){
+                if (inputArr[0].equals("start")){
+                    if (inputArr[1].equals("easy") || inputArr[1].equals("medium") || inputArr[1].equals("hard") || inputArr[1].equals("user")){
+                        if (inputArr[2].equals("easy") || inputArr[2].equals("medium") || inputArr[2].equals("hard") || inputArr[2].equals("user")){
+                            break;
+                        }
+                    }
+                }
+            }
+            System.out.println("Bad parameters!");
+        }
+        printField(field);
+
+        // --- start the game!!! -----
+
+        String first;
+        String second;
+        if (inputArr[1].equals("user")){
+            first = "user";
+        } else{
+            first = "bot";
+        }
+        if (inputArr[2].equals("user")){
+            second = "user";
+        } else{
+            second = "bot";
+        }
+
+        Robot robot1 = new Robot(inputArr[1], 'X');
+        Robot robot2 = new Robot(inputArr[2], 'O');
+
+
+        while(!checkWin(field)){
+            if (first.equals("user")){
+                set(field, scanner);
+            } else{
+                System.out.printf("Making move level \"%s\"\n", robot1.mode);
+                robot1.makeMove(field);
+            }
+            printField(field);
+            if (checkWin(field)){
+                break;
+            }
+
+            if (second.equals("user")){
+                set(field, scanner);
+            } else{
+                System.out.printf("Making move level \"%s\"\n", robot2.mode);
+                robot2.makeMove(field);
+            }
+            printField(field);
+        }
+    }
+
+    public static char getCurrent(char[][] field){
+        int xs = 0;
+        int os = 0;
+        for (int i = 0; i < field.length; i++){
+            for (int j = 0; j < field[i].length; j++){
+                if (field[i][j] == 'X'){
+                    xs++;
+                }
+                if (field[i][j] == 'O'){
+                    os++;
+                }
+            }
+        }
+        if (xs == os){
+            return 'X';
+        } else{
+            return 'O';
+        }
+    }
+    public static char[][] createField(){
+        char[][] field = new char[3][3];
+        for (int i = 0; i < field.length; i++){
+            for (int j = 0; j < field[i].length; j++){
+                field[i][j] = ' ';
+            }
+        }
+        return field;
+    }
+    public static boolean checkWin(char[][] field){
+        char winChar = 'd';
+
+        // --- game not finished control ---
+
+        int counSpace = 0;
+        for (int i = 0; i < field.length; i++){
+            for (int j = 0; j < field[i].length; j++){
+                if (field[i][j] == ' '){
+                    counSpace++;
+                    break;
+                }
+            }
+        }
+        if (counSpace == 0){
+            System.out.println("Draw");
+            return true;
+        }
+        // --- normal control ----
+
+        for (int i = 0; i < field.length; i++) {
+            int count1 = 0;
+            int count2 = 0;
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[0][i] == field[j][i] && field[0][i] != ' ') {
+                    count1++;
+                }
+                if (field[i][0] == field[i][j] && field[i][0] != ' ') {
+                    count2++;
+                }
+            }
+            if (count1 == field.length) {
+                winChar = field[0][i];
+            } else if (count2 == field[i].length) {
+                winChar = field[i][0];
+            }
+        }
+
+        // --- cross control ----
+
+
+            int count1 = 0;
+            for (int j = 0; j < field.length; j++){
+                if (field[0][0] == field[j][j] && field[j][j] != ' '){
+                    count1++;
+                }
+            }
+            if (count1 == field.length){
+                winChar = field[0][0];
+            }
+
+
+        // second
+        // TODO HERE IS ERROR
+        /*
+        _ _ X
+        _ X _
+        X _ _
+         */
+        // TODO paste this to robot
+        int count2 = 0;
+        for (int i = 0; i < field.length; i++){
+            if (field[i][field.length - 1 - i] == field[0][field.length - 1] && field[i][field.length - 1 - i] != ' ') {
+                count2++;
+            }
+        }
+        if (count2 == field.length){
+            winChar = field[field.length - 1][0];
+        }
+
+        // --- who won -----
+        if (winChar != 'd'){
+            System.out.println(winChar + " wins");
+            return true;
+        }
+        return false;
+    }
+
+    public static void printField(char[][] field){
+        System.out.println("---------");
+        for (int i = 0; i < field.length; i++){
+            System.out.print("| ");
+            for (int j = 0; j < field[i].length; j++){
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+    public static void set(char[][] field, Scanner scanner){
+        // --- isValid check ---
+        boolean again;
+        int row = 0;
+        int col = 0;
+        while(true){
+            again = false;
+            System.out.print("Enter the coordinates: ");
+            try {
+                row = scanner.nextInt() - 1;
+                col = scanner.nextInt() - 1;
+
+                if (row >= 3 || col >= 3 || row < 0 || col < 0){
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    again = true;
+                } else if (field[row][col] != ' '){
+                    System.out.println("This cell is occupied! Choose another one!");
+                    again = true;
+                }
+            } catch (Exception e){
+                System.out.println("You should enter numbers!");
+                scanner.nextLine();
+                again = true;
+            }
+
+            if (!again){
+                break;
+            }
+        }
+        field[row][col] = getCurrent(field);
+    }
+
+}
---
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/Robot.java
@@ -0,0 +1,276 @@
+package tictactoe;
+
+import java.util.Random;
+
+public class Robot {
+    String mode;
+    char symbol;
+    char oponentSymbol;
+
+    Robot(String mode, char symbol) throws Exception {
+        if (mode.equals("easy") || mode.equals("medium") || mode.equals("hard")) {
+            this.mode = mode;
+        } /* else{
+            System.out.println("Error in class Robot, constructor");
+        } */
+        //TODO There is probably error in equals
+        this.symbol = symbol;
+        if (symbol == 'X') {
+            oponentSymbol = 'O';
+        } else {
+            oponentSymbol = 'X';
+        }
+    }
+
+    public void makeMove(char[][] field) {
+        if (mode.equals("easy")) {
+            easyMove(field);
+        } else if (mode.equals("medium")) {
+            mediumMove(field);
+        } else if (mode.equals("hard")) {
+            hardMove(field);
+        }
+    }
+
+    public void hardMove(char[][] field) {
+        int bestScoreX = -9999;
+        int bestScoreO = 9999;
+        boolean isMaximizing = symbol == 'X';
+        int[] bestMove = null;
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == ' ') {
+                    field[i][j] = symbol;
+                    int score = minimax(field, 0, !isMaximizing);
+                    field[i][j] = ' ';
+                    if ((symbol == 'X' && score > bestScoreX)) {
+                        bestScoreX = score;
+                        bestMove = new int[]{i, j};
+                    }
+                    if ((symbol == 'O' && score < bestScoreO)) {
+                        bestScoreO = score;
+                        bestMove = new int[]{i, j};
+                    }
+
+                }
+            }
+        }
+        if (bestMove != null){
+            field[bestMove[0]][bestMove[1]] = symbol;
+        }
+    }
+
+    public int minimax(char[][] field, int depth, boolean isMaximizingPlayer) {
+        String boardValue = fieldValue(field);
+        if (!boardValue.equals("nothing")){
+            if (boardValue.equals("X")){
+                return 10;
+            } else if (boardValue.equals("O")) {
+                return -10;
+            } else if (boardValue.equals("tie")) {
+                return 0;
+            }
+        }
+
+        if (isMaximizingPlayer) {
+            int bestVal = -9999;
+            for (int i = 0; i < field.length; i++) {
+                for (int j = 0; j < field[i].length; j++) {
+                    if (field[i][j] == ' ') {
+                        field[i][j] = 'X';
+                        int value = minimax(field, depth + 1, false);
+                        field[i][j] = ' ';
+                        bestVal = Math.max(bestVal, value);
+                    }
+                }
+            }
+            return bestVal;
+        } else {
+            int bestVal = 9999;
+            for (int i = 0; i < field.length; i++) {
+                for (int j = 0; j < field[i].length; j++) {
+                    if (field[i][j] == ' ') {
+                        field[i][j] = 'O';
+                        int value = minimax(field, depth + 1, true);
+                        field[i][j] = ' ';
+                        bestVal = Math.min(bestVal, value);
+                    }
+
+                }
+            }
+            return bestVal;
+        }
+    }
+
+    public void mediumMove(char[][] field) {
+
+        // col
+        for (int i = 0; i < field.length; i++) {
+            int sumYou = 0;
+            int sumOpponent = 0;
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == symbol) {
+                    sumYou++;
+                } else if (field[i][j] == oponentSymbol) {
+                    sumOpponent++;
+                }
+            }
+            if (sumYou == field.length - 1 || sumOpponent == field.length - 1) {
+                for (int k = 0; k < field[i].length; k++) {
+                    if (field[i][k] == ' ') {
+                        field[i][k] = symbol;
+                        return;
+                    }
+                }
+            }
+        }
+        // row
+        for (int i = 0; i < field.length; i++) {
+            int sumYou = 0;
+            int sumOpponent = 0;
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[j][i] == symbol) {
+                    sumYou++;
+                } else if (field[j][i] == oponentSymbol) {
+                    sumOpponent++;
+                }
+            }
+            if (sumYou == field.length - 1 || sumOpponent == field.length - 1) {
+                for (int k = 0; k < field[i].length; k++) {
+                    if (field[k][i] == ' ') {
+                        field[k][i] = symbol;
+                        return;
+                    }
+                }
+            }
+        }
+
+        // left up to right down
+        while (true) {
+            int sumYou = 0;
+            int sumOpponent = 0;
+            for (int i = 0; i < field.length; i++) {
+                if (field[i][i] == symbol) {
+                    sumYou++;
+                } else if (field[i][i] == oponentSymbol) {
+                    sumOpponent++;
+                }
+            }
+            if (sumYou == field.length - 1 || sumOpponent == field.length - 1) {
+                for (int k = 0; k < field.length; k++) {
+                    if (field[k][k] == ' ') {
+                        field[k][k] = symbol;
+                        return;
+                    }
+                }
+            }
+            break;
+        }
+        // left down to right up
+        while (true) {
+            int sumYou = 0;
+            int sumOpponent = 0;
+            for (int i = 0; i < field.length; i++) {
+                if (field[field.length - 1 - i][i] == symbol) {
+                    sumYou++;
+                } else if (field[field.length - 1 - i][i] == oponentSymbol) {
+                    sumOpponent++;
+                }
+            }
+            if (sumYou == field.length - 1 || sumOpponent == field.length - 1) {
+                for (int k = 0; k < field.length; k++) {
+                    if (field[field.length - 1 - k][k] == ' ') {
+                        field[field.length - 1 - k][k] = symbol;
+                        return;
+                    }
+                }
+            }
+            break;
+        }
+        easyMove(field);
+    }
+
+    public void easyMove(char[][] field) {
+        Random random = new Random();
+        int row = random.nextInt(field.length);
+        int col = random.nextInt(field.length);
+        while (field[row][col] != ' ') {
+            row = random.nextInt(field.length);
+            col = random.nextInt(field.length);
+        }
+        field[row][col] = symbol;
+    }
+
+    public static String fieldValue(char[][] field) {
+        char winChar = 'n';
+
+        // --- normal control ----
+
+        for (int i = 0; i < field.length; i++) {
+            int count1 = 0;
+            int count2 = 0;
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[0][i] == field[j][i] && field[0][i] != ' ') {
+                    count1++;
+                }
+                if (field[i][0] == field[i][j] && field[i][0] != ' ') {
+                    count2++;
+                }
+            }
+            if (count1 == field.length) {
+                winChar = field[0][i];
+            } else if (count2 == field[i].length) {
+                winChar = field[i][0];
+            }
+        }
+
+
+        // --- cross control ----
+
+        int count3 = 0;
+        for (int j = 0; j < field.length; j++) {
+            if (field[0][0] == field[j][j] && field[j][j] != ' ') {
+                count3++;
+            }
+        }
+        if (count3 == field.length) {
+            winChar = field[0][0];
+        }
+
+
+        // second
+        int count4 = 0;
+        for (int i = 0; i < field.length; i++) {
+            if (field[i][field.length - 1 - i] == field[0][field.length - 1] && field[i][field.length - 1 - i] != ' ') {
+                count4++;
+            }
+        }
+        if (count4 == field.length) {
+            winChar = field[field.length - 1][0];
+        }
+
+        int openSpots = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (field[i][j] == ' ') {
+                    openSpots++;
+                }
+            }
+        }
+
+        // --- who won -----
+
+        // TODO maybe you need to return 10 or - 10 with symbol now by 'X' or 'O'
+
+        // 5
+        if (winChar == 'n' && openSpots == 0) {
+            return "tie";
+        } else if (winChar == 'n') {
+            return "nothing";
+        }
+        return String.valueOf(winChar);
+    }
+}
+
+
